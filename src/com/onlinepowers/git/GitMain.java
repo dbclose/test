65f37f7d363a8cd43868294c1f889810eeba219f
SKC <skc@onlinepowers.com>
2013-02-19 11:33:08 +0900

    버그수정

--- src/com/onlinepowers/git/GitMain.java
+++ src/com/onlinepowers/git/GitMain.java
@@ -7,6 +7,11 @@ public class GitMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("XXX");
+		System.out.println("XXX");
+		System.out.println("XXX");
+		System.out.println("XXX");
+		System.out.println("XXX");
+		System.out.println("버그수정!!!");
 
 	}
 
